a1cc712137e69db2922a57e56c7f12df231d3a3a
CutaiaMattia <matcut94@gmail.com>
2022-11-22 17:16:19 +0100

    22/11 PM

---
+++ ImplemantationInterface/src/main/java/Persona.java
@@ -0,0 +1,42 @@
+public class Persona implements Comparable<Persona> {
+    private String nome;
+    private String cognome;
+    private int eta;
+
+    public Persona() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    @Override
+    public int compareTo(Persona o) {
+        if (o.getEta() < this.eta) {
+            return -1;
+        } else if (o.getEta() > this.eta) {
+            return 1;
+        } else
+            return 0;
+    }
+}
