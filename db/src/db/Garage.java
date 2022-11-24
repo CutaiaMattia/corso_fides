ec687aeea8b2a31f92125e581bb4b8b416022c8e
CutaiaMattia <matcut94@gmail.com>
2022-11-24 10:43:58 +0100

    24/11 complete garage with scanner and all CRUD methods

--- db/src/db/Garage.java
+++ db/src/db/Garage.java
@@ -7,7 +7,6 @@ import java.util.List;
 public class Garage {
 
 
-
     Connection conn = null;
 
 
@@ -22,16 +21,15 @@ public class Garage {
     }
 
 
-
     public void insertCar(String plate, String type, String producer) {
         Statement st = null;
 
         int ri = 0;
-        String query = String.format("INSERT INTO veicolo VALUES(%s,%s,%s)", plate, type, producer);
+        String query = String.format("INSERT INTO veicolo VALUES('%s','%s','%s')", plate, type, producer);
         try {
             st = conn.createStatement();
             ri = st.executeUpdate(query);
-            System.out.printf("righe inserite = %i", ri);
+            System.out.printf("righe inserite = %s", ri);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -61,25 +59,30 @@ public class Garage {
         }
     }
 
-    public void delete( String value) {
-
-
-        ResultSet rs = null;
-        String query = "DELETE FROM veicolo";
+    public void delete(String value) {
+        int rd= 0;
+        String query = String.format("DELETE FROM veicolo WHERE plate='%s'",value);
         Statement st = null;
 
         try {
             st = conn.createStatement();
-            rs = st.executeQuery(query);
-            ResultSetMetaData rsmd = rs.getMetaData();
+            rd = st.executeUpdate(query);
 
-            while (rs.next()) {
-                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                    System.out.println(rsmd.getColumnName(i) + " : " + rs.getString(i));
-                }
-                System.out.println();
+            System.out.printf("righe cancellate = %s", rd);
 
-            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void upDateCar(String updateColumn, String value, String plate) {
+        Statement st = null;
+        int rm = 0;
+        String query = String.format("UPDATE veicolo SET %s='%s' WHERE plate='%s'",updateColumn,value,plate );
+        try {
+            st = conn.createStatement();
+            rm = st.executeUpdate(query);
+            System.out.printf("righe modificate = %s", rm);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- db/src/db/Main.java
+++ db/src/db/Main.java
@@ -6,25 +6,54 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        Garage g = new Garage();
-        switch (input) {
-            case "show": {
-                g.showGarage();
-                break;
-            }
-            case "insert" : {
-                System.out.println("plate");
-                String plate = in.nextLine();
-                System.out.println("type");
-                String type = in.nextLine();
-                System.out.println("producer");
-                String producer = in.nextLine();
-                g.insertCar(plate,type,producer);
-                break;
+        boolean isClose = false;
+        while (!isClose) {
+            System.out.println("what do you want to do ? [Show / insert / delete / update / stop]");
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine().toLowerCase();
+            Garage g = new Garage();
+            switch (input.trim()) {
+                case "show": {
+                    g.showGarage();
+                    break;
+                }
+                case "insert": {
+                    System.out.println("insert : ");
+                    System.out.println("plate ");
+                    String plate = in.nextLine();
+                    System.out.println("type ");
+                    String type = in.nextLine();
+                    System.out.println("producer ");
+                    String producer = in.nextLine();
+                    g.insertCar(plate, type, producer);
+                    break;
+                }
+
+                case "delete": {
+                    System.out.println("insert plate to delete");
+                    String plate = in.nextLine();
+                    g.delete(plate);
+                    break;
+                }
+
+                case "update": {
+                    System.out.println("insert : ");
+                    System.out.println("column to update ");
+                    String column = in.nextLine().toLowerCase();
+                    System.out.println("value ");
+                    String value = in.nextLine();
+                    System.out.println("plate to update ");
+                    String plate = in.nextLine();
+                    g.upDateCar(column, value, plate);
+                    break;
+                }
+                case "stop": {
+                    isClose = true;
+                }
+                default:
+                    System.out.println("Show / insert / delete / update / stop");
+                    break;
             }
         }
-
     }
 }
